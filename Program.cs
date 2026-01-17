9f34c7b8645f6f51483d7608e79e507c3e14a8de
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-01-17 16:51:20 +0100

    basic server CLI

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿namespace P2PShare.Server
+{
+    class Program
+    {
+        static async void Main()
+        {
+            string command;
+
+            do
+            {
+                command = await CLIHandler.GetCommand();
+
+                switch (command)
+                {
+                    case "start":
+                        // Start server logic here
+                        break;
+                    case "stop":
+                        // Stop server logic here
+                        break;
+                    case "exit":
+                        // dispose resources here
+                        break;
+                    
+                    default:
+                        // display help probably
+                        break;
+                }
+            }
+            while (command != "exit");
+        }
+    }
+}
