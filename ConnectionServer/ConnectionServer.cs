fccedda9095346c9a53ce423153ad4e597b655c2
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-02 23:48:12 +0100

    ConnectionServer class + ConnectionServerHandler moved

---
+++ ConnectionServer/ConnectionServer.cs
@@ -0,0 +1,19 @@
+﻿namespace P2PShare.Server.ConnectionServer
+{
+    public class ConnectionServer(CancellationToken cancellationToken) : IDisposable
+    {
+        private ConnectionServerHandler _connectionHandler = new(cancellationToken);
+        private Task? _communication;
+
+        public void Dispose() => _connectionHandler.Dispose();
+
+        public void Serve() => _communication = ServeLoopAsync();
+
+        public async void InitAsync() => await _connectionHandler.WaitForConnectionAsync();
+
+        private async Task ServeLoopAsync()
+        {
+            // this will handle the client requests
+        }
+    }
+}
