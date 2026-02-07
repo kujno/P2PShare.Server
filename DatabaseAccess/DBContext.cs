aefb5502e3a2e708800aa5a1b3f72542f3924b2c
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-07 20:26:14 +0100

    DB files renamed

---
+++ DatabaseAccess/DBContext.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace P2PShare.Server.DatabaseAccess
+{
+    public static class DBContext
+    {
+        private static CancellationToken _cancellationToken;
+        private static string? _connectionString;
+
+        public static async Task InitAsync(CancellationToken cancellationToken)
+        {
+            DBCredentials credentials;
+
+            _cancellationToken = cancellationToken;
+            credentials = await DBCredentials.GetAsync(cancellationToken);
+            _connectionString = $"Server={credentials.Server};Database={credentials.Database};User ID={credentials.UserID};Password={credentials.Password};";
+        }
+
+        public static async Task AddUserAsync(string username, string hash, string name, string surename) => await ExecCommand($"insert into users (username, password_hash, name, surename) values (\"{username}\", \"{hash}\", \"{name}\", \"{surename}\");", false);
+
+        public static async Task<string[]> GetUsernamesAsync()
+        {
+            List<string> usernames = new();
+
+            using (MySqlDataReader reader = (await ExecCommand("select username from users;", true))!)
+            {
+                while (await reader.ReadAsync(_cancellationToken))
+                    usernames.Add(reader.GetString("username"));
+            }
+
+            return usernames.ToArray();
+        }
+
+        public static async Task<string?> GetPasswordHashAsync(string username)
+        {
+            using (MySqlDataReader reader = (await ExecCommand($"select password_hash from users where username = \"{username}\";", true))!)
+            {
+                if (await reader.ReadAsync(_cancellationToken))
+                    return reader.GetString("password_hash");
+            }
+
+            return null;
+        }
+
+        private static async Task<MySqlDataReader?> ExecCommand(string commandString, bool query)
+        {
+            using (MySqlConnection connection = new(_connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new(commandString, connection))
+                {
+                    if (query)
+                        return (MySqlDataReader)await command.ExecuteReaderAsync();
+
+                    await command.ExecuteNonQueryAsync(_cancellationToken);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ DatabaseAccess/DBCredentials.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace P2PShare.Server.DatabaseAccess
+{
+    public class DBCredentials
+    {
+        public static string DBCredentialsFileName { get; } = "DBCredentials.json";
+
+        public string? Server { get; init; }
+        public string? Database { get; init; }
+        public string? UserID { get; init; }
+        public string? Password { get; init; }
+
+        public static async Task<DBCredentials> GetAsync(CancellationToken cancellationToken)
+        {
+            DBCredentials credentials;
+
+            try
+            {
+                var json = await File.ReadAllTextAsync(DBCredentialsFileName, cancellationToken);
+                credentials = JsonConvert.DeserializeObject<DBCredentials>(json)!;
+            }
+            catch (Exception ex)
+            {
+                throw ex is OperationCanceledException ?
+                    ex :
+                    new FormatException($"Failed to convert {DBCredentialsFileName}.", ex);
+            }
+
+            return credentials;
+        }
+
+        public static async Task SaveToFileAsync(string server, string database, string userID, string password)
+        {
+            using (var stream = new FileStream(DBCredentialsFileName, FileMode.Create))
+                await stream.WriteAsync(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new DBCredentials()
+                {
+                    Server = server,
+                    Database = database,
+                    UserID = userID,
+                    Password = password
+                })));
+        }
+    }
+}
