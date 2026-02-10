21916359d7c4693f2bc338593049afc0b631b580
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-10 22:34:03 +0100

    wip

--- DBAccess/DBContext.cs
+++ DBAccess/DBContext.cs
@@ -15,13 +15,43 @@ namespace P2PShare.Server.DBAccess
 
         public static async Task AddUserAsync(string username, string hash, string name, string surename)
         {
+            var tag = "id";
+            int? idUser = null, idGroup = null;
+
             await ExecNonQueryAsync(new string[]
             {
                 $"INSERT INTO users (username, password_hash, name, surename) VALUES (\"{username}\", \"{hash}\", \"{name}\", \"{surename}\");",
-                $"INSERT INTO usergroups (name, isuser) VALUES (\"{username}\", 1)"
+                $"INSERT INTO usergroups (name, isuser) VALUES (\"{username}\", 1);"
             });
 
-            // add to the many to many table
+
+
+            //// wip
+            //using (MySqlConnection connection = new(_connectionString))
+            //{
+            //    connection.Open();
+
+            //    using (MySqlCommand command = new($"SELECT {tag} FROM users WHERE username = \"{username}\";", connection))
+            //    {
+            //        using (var reader = (MySqlDataReader)await command.ExecuteReaderAsync())
+            //        {
+            //            while (await reader.ReadAsync())
+            //            {
+            //                idUser = reader.GetInt32(tag);
+            //            }
+            //        }
+            //    }
+            //}
+
+            //using (var reader = await ExecQueryAsync($"SELECT {tag} FROM usergroups WHERE isuser = 1 && name = \"{username}\";"))
+            //{
+            //    while (await reader.ReadAsync())
+            //    {
+            //        idGroup = reader.GetInt32(tag);
+            //    }
+            //}
+
+            //await ExecNonQueryAsync($"INSERT INTO usergroups_has_users (usergroups_id, users_id) VALUES ({idGroup}, {idUser})");
         }
 
         public static async Task<string[]> GetUsernamesAsync()
@@ -86,6 +116,52 @@ namespace P2PShare.Server.DBAccess
                     }
                 }
             }
+        }
+
+        private static async Task<Dictionary<string, string>[]> ExecQueryAsync<T>(T columns, string table, string? condition)
+        {
+            var tType = columns?.GetType();
+            string[] columnsArr;
+
+            if (tType == typeof(string[]))
+                columnsArr = (string[])(object)columns!;
+            else if (tType == typeof(string))
+                columnsArr = new string[] { (string)(object)columns! };
+            else
+                throw new NotImplementedException();
+
+            List<Dictionary<string, string>> values = [];
+            string columnsStr = String.Empty;
+
+            for (var i = 0; i < columnsArr.Length; i++)
+            {
+                columnsStr += columnsArr[i];
+                if (i < columnsArr.Length - 1)
+                    columnsStr += ", ";
+            }
+
+            using (MySqlConnection connection = new(_connectionString))
+            {
+                await connection.OpenAsync(_cancellationToken);
+
+                using (MySqlCommand command = new($"SELECT DISTINCT {columnsStr} FROM {table}{(condition is not null ? $" WHERE {condition}" : String.Empty)};"))
+                {
+                    using (var reader = (MySqlDataReader)await command.ExecuteReaderAsync(_cancellationToken))
+                    {
+                        while (!_cancellationToken.IsCancellationRequested && await reader.ReadAsync(_cancellationToken))
+                        {
+                            Dictionary<string, string> row = [];
+
+                            foreach (var column in columnsArr)
+                                row.Add(column, reader.GetString(column));
+
+                            values.Add(row);
+                        }
+                    }
+                }
+            }
+
+            return values.ToArray();
         }
     }
 }
