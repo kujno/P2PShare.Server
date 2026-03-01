467a4e0f9ffecafd5dd7bbea0a580f6f3a79a2b6
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-03-01 01:43:43 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -179,7 +179,7 @@ namespace P2PShare.Server
 
                     await DBContext.ExecNonQueryAsync("DELETE FROM users WHERE username = \"admin\";");
 
-                    await DBContext.AddUserAsync("admin", Hasher.Hash(GetString("Usernam is admin.\nPassword")), String.Empty, String.Empty);
+                    await DBContext.AddUserAsync("admin", Hasher.Hash(GetString("Username is admin.\nPassword")), String.Empty, String.Empty);
 
                     DisplayCommandOutput("New admin credentials created.");
 
